253ab3ec4f7e483a715faac60d737bd936c04766
wegar-2 <prometheus0209@gmail.com>
2017-09-30 11:43:17 +0200

    Finished the 13.1 lesson.

--- 13.1_function_templates/TestTemplFun.hpp
+++ 13.1_function_templates/TestTemplFun.hpp
@@ -1,6 +1,24 @@
 #ifndef TESTTEMPLFUN_HPP
 #define TESTTEMPLFUN_HPP
 
-template<typename T1, typename T2> T1 TestTemplFun(T2 ArgIn);
+/* ********************************************************** */
+
+#include <iostream>
+
+using namespace std;
+
+template <typename T1, typename T2> T1 TestTemplFun(T2 ArgIn);
+
+template<typename T1, typename T2>
+T1 TestTemplFun(T2 ArgIn)
+{
+    cout << "Inside TestTemplFun..." << endl;
+    cout << "Argument passed to TestTemplFun: " << ArgIn << endl;
+    cout << "Quitting TestTemplFun..." << endl;
+
+    return 99;
+}
+
+/* ********************************************************** */
 
 #endif // TESTTEMPLFUN_HPP
--- 13.1_function_templates/main.cpp
+++ 13.1_function_templates/main.cpp
@@ -6,16 +6,21 @@ using namespace std;
 template <typename T1>
 void TestFunctionOne(T1 Arg1);
 
+template <typename T1, typename T2>
+T1 AnotherTestFun(T2 arg);
 
 int main()
 {
     // 1. simplest example possible
     TestFunctionOne(1.23);
     TestFunctionOne<double>(1);
-    // 2.
+    // 2. when using a template function included from the header file
+    //    it is necessary to remember that the implementation of a template
+    //    needs to be put into the header file.
     TestTemplFun<int, double>(123.23);
     // 3.
-
+    AnotherTestFun<int, int>(123);
+    AnotherTestFun<int, double>(123.123);
     return 0;
 }
 
@@ -24,3 +29,14 @@ void TestFunctionOne(T1 Arg1) {
     cout << "InsideTestFunctionOne..." << endl;
     cout << "Arg1 = " << Arg1 << endl;
 }
+
+template <typename T1, typename T2>
+T1 AnotherTestFun(T2 arg)
+{
+    cout << "arg: " << arg << endl;
+    T1 arg2 = static_cast<T1>(arg);
+    cout << "arg2: " << arg2 << endl;
+    return 12;
+}
+
+
