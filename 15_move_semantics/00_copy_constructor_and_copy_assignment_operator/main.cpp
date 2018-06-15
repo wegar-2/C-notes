83a0f7348261290bff2ee37465c95e9f29d16c44
wegar-2 <prometheus0209@gmail.com>
2018-06-15 12:27:33 +0200

    Added a check of a few more cases in which copy constructor and assignment operator might be called.

--- 15_move_semantics/00_copy_constructor_and_copy_assignment_operator/main.cpp
+++ 15_move_semantics/00_copy_constructor_and_copy_assignment_operator/main.cpp
@@ -13,5 +13,10 @@ int main()
     Point2D obj3 = Point2D(123.3);
     cout << "assigning obj3 to obj1" << endl;
     obj1 = obj3;
+    cout << endl << endl << endl;
+    cout << "yet another case: " << endl;
+    Point2D* obj4 = new Point2D(obj3); // this calls a copy constructor
+    cout << "also, let's see what happens here: " << endl;
+    *obj4 = Point2D(obj2); // first copy constructor and assignment operator are called here
     return 0;
 }
